e7da0f31a0a4cf4d9b16b4bd03a8ce6a8b8e1ef1
gwang <rhdl142@naver.com>
2018-09-28 23:54:03 +0900

    access 파트

--- Notice/src/com/test/notice/access/UserAccessDTO.java
+++ Notice/src/com/test/notice/access/UserAccessDTO.java
@@ -1,5 +1,29 @@
 package com.test.notice.access;
 
 public class UserAccessDTO {
-
+	private String seq;
+	private String userSeq;
+	private String noticeSeq;
+	public String getSeq() {
+		return seq;
+	}
+	public void setSeq(String seq) {
+		this.seq = seq;
+	}
+	public String getUserSeq() {
+		return userSeq;
+	}
+	public void setUserSeq(String userSeq) {
+		this.userSeq = userSeq;
+	}
+	public String getNoticeSeq() {
+		return noticeSeq;
+	}
+	public void setNoticeSeq(String noticeSeq) {
+		this.noticeSeq = noticeSeq;
+	}
+	@Override
+	public String toString() {
+		return "UserAccessDTO [seq=" + seq + ", userSeq=" + userSeq + ", noticeSeq=" + noticeSeq + "]";
+	}
 }
